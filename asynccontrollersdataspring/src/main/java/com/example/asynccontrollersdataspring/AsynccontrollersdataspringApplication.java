ac684cf49d5f6523f7c64597b5e09b2f9bbda030
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2021-12-22 21:25:32 -0300

    Add files via upload

---
+++ asynccontrollersdataspring/src/main/java/com/example/asynccontrollersdataspring/AsynccontrollersdataspringApplication.java
@@ -0,0 +1,38 @@
+package com.example.asynccontrollersdataspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+
+@SpringBootApplication
+@ComponentScan({"com.example.asynccontrollersdataspring.repositories"})
+@EnableAsync
+public class AsynccontrollersdataspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynccontrollersdataspringApplication.class, args);
+	}
+
+//	@Bean
+//	public Executor taskExecutor(){
+//		return ForkJoinPool.commonPool();
+//	}
+//
+//	@Bean
+//	public Executor cachedThreadPool(){
+//		return Executors.newCachedThreadPool();
+//	}
+//
+//
+//	@Bean
+//	public Executor customThreadPool() {
+//		return ForkJoinPool.commonPool();
+//
+//	}
+}
